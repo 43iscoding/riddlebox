fb71d4f77a8125b929c94e9285ea7bad27f94e97
Vsevolod Golovin <43iscoding@gmail.com>
2015-03-27 23:44:04 +0200

    basic rotation

---
+++ Assets/Scripts/Rotatable.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotatable : MonoBehaviour {
+	
+	public float rotationSpeed;
+
+	private Vector3 prevMousePosition;
+	private Vector3 currentMousePosition;
+
+	private bool dragging;
+
+	void Start() {
+		prevMousePosition = currentMousePosition = Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		prevMousePosition = currentMousePosition;
+		currentMousePosition = Input.mousePosition;
+
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hitInfo;
+			Physics.Raycast (ray, out hitInfo);
+			if (hitInfo.collider == null || hitInfo.collider.tag != "Widget") {
+				dragging = true;
+			} else {
+				dragging = false;
+			}
+		}
+
+		if (!Input.GetMouseButton (0)) {
+			dragging = false;
+		}
+
+		if (dragging) {
+			Vector3 delta = currentMousePosition - prevMousePosition;
+			Debug.Log (delta);
+			transform.Rotate(delta);
+		}
+
+
+
+	}
+}
