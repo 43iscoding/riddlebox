3cf7fdd6bc6142cb5ec97e46d2ede9b9186835f5
kindex <kindex@kindex.lv>
2015-03-27 23:08:50 +0200

    Unity5

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -1,15 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Box : MonoBehaviour {
+public class Box : MonoBehaviour 
+{
+	//public List<>
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
