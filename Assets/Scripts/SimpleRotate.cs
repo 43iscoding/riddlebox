86aa62e63a2f8c0d3f25aeece29cd2abb7a774bd
kindex <kindex@kindex.lv>
2015-03-28 20:05:53 +0200

    Small tuning

---
+++ Assets/Scripts/SimpleRotate.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour
+{
+	const float rotationSpeed = 0.3f;
+
+	Vector3 prevMousePosition;
+	Vector3 currentMousePosition;
+
+	void Start()
+	{
+		prevMousePosition = currentMousePosition = Input.mousePosition;
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log("OnMouseDown");
+		prevMousePosition = currentMousePosition;
+	}
+
+	void OnMouseDrag()
+	{
+		prevMousePosition = currentMousePosition;
+		currentMousePosition = Input.mousePosition;
+
+		Vector3 delta = currentMousePosition - prevMousePosition;
+		Vector3 rotation = new Vector3(delta.y, -delta.x);
+		Debug.Log(rotation);
+		transform.Rotate(rotation * rotationSpeed, Space.Self);
+	}
+}
