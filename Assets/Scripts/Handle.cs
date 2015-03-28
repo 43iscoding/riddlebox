6c8bd9a5346d5c13c88ad3d0201f53cab1ce17c7
kindex <kindex@kindex.lv>
2015-03-28 11:29:23 +0200

    Handle

---
+++ Assets/Scripts/Handle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Handle : MonoBehaviour
+{
+	public float needAngle;
+	public float treshhold;
+
+	public bool IsOk()
+	{
+		return transform.
+	}
+
+	public void Update()
+	{
+	}
+}
