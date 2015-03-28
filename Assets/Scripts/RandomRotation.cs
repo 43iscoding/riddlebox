54d8e338340e7ae246fa7d46ff6aac4ed5f45568
Andrejs Birzgals <andrejs.birzgals@gmail.com>
2015-03-28 17:37:40 +0200

    ui stuff for main menu

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotation : MonoBehaviour {
+
+	void Update()
+	{
+		transform.RotateAround(transform.position, Vector3.up, 5 * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/RotateSkybox.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class RotateSkybox : MonoBehaviour
+{
+	
+	void Update()
+	{
+		// Construct a rotation matrix and set it for the shader
+		float m = (-Time.realtimeSinceStartup*3 % 360);
+		Debug.Log(m);
+		GetComponent<Skybox>().material.SetFloat("_Rotation", m);
+	}
+
+}
