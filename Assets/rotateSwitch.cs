0763efac0f616cb2e2ad3fc27ffb130028788a4e
kindexz <alexeyvolovik@lu-wifi468.lanet.lv>
2015-03-28 21:01:38 +0200

    1
    1

---
+++ Assets/rotateSwitch.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class rotateSwitch : MonoBehaviour 
+{
+	static int locks = 0;
+	public bool vertical;
+	public bool complete;
+	
+	public List<GameObject> switchesToTurn;
+	public List<GameObject> allSwitches;
+
+	// Use this for initialization
+	void Start () {
+		vertical = true;
+		complete = false;
+	}
+	
+	public void TurnSwitch()
+	{
+		StartCoroutine(TweenRotate());
+	//	transform.RotateAround(transform.position, transform.forward, 90);
+		vertical = !vertical;	
+	}
+	
+	IEnumerator TweenRotate()
+	{
+		locks++;
+		Timer t = new Timer(0.3f);
+		Quaternion from = transform.localRotation;
+		Quaternion to;
+		if (vertical)
+		{
+			to = Quaternion.Euler(0,0,90);
+		}
+		else
+		{
+			to = Quaternion.Euler(0,0,0);			
+		}
+		
+
+		while (!t.IsEnded())
+		{
+		
+			transform.localRotation = Quaternion.Lerp(from, to, t.GetCompletePercent());
+			
+			yield return null;
+		}
+		transform.localRotation =  to;
+		locks--;
+	}
+	
+	void OnMouseDown()
+	{
+		if (locks == 0)
+		{	
+			TurnSwitch();
+			
+			for (int i = 0; i < switchesToTurn.Count; i++)
+			{
+				switchesToTurn[i].GetComponent<rotateSwitch>().TurnSwitch();
+			}
+			
+			rotateSwitch switch1 = allSwitches[1].GetComponent<rotateSwitch>();
+			rotateSwitch switch3 = allSwitches[3].GetComponent<rotateSwitch>();
+			rotateSwitch switch5 = allSwitches[5].GetComponent<rotateSwitch>();
+			rotateSwitch switch7 = allSwitches[7].GetComponent<rotateSwitch>();
+					
+			if (!switch1.vertical && switch3.vertical && switch5.vertical && !switch7.vertical)
+			{
+				for (int i = 0; i < allSwitches.Count; i++)
+				{
+					allSwitches[i].GetComponent<rotateSwitch>().complete = true;
+			    }
+			    	
+			    print ("complete");
+			}
+		}
+	}
+}
